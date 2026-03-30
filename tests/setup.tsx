f206fd79519870d9f3b3932d45508067cf5502c3
Hector Luis Lorenzo Zaldivar <lorenzozaldivar2023@gmail.com>
2026-03-30 21:39:05 +0200

    fix(test): resolve all eslint errors and reorder pre-commit hooks

--- tests/setup.tsx
+++ tests/setup.tsx
@@ -56,54 +56,40 @@ vi.mock('next/navigation', () => ({
 // Mock next/image
 vi.mock('next/image', () => ({
   __esModule: true,
-  default: (props: any) => {
+  default: function NextImage({ fill, ...props }: React.ImgHTMLAttributes<HTMLImageElement> & { fill?: boolean | string }) {
     // eslint-disable-next-line @next/next/no-img-element
-    return <img {...props} fill={props.fill ? "true" : undefined} />;
+    return <img {...props} data-fill={fill ? "true" : undefined} alt={props.alt || ""} />;
   },
 }));
 
 // Mock framer-motion to simplify rendering for unit tests
-vi.mock('framer-motion', () => ({
-  motion: {
-    div: React.forwardRef(({ children, ...props }: any, ref) => (
-      <div {...props} ref={ref}>{children}</div>
-    )),
-    h1: React.forwardRef(({ children, ...props }: any, ref) => (
-      <h1 {...props} ref={ref}>{children}</h1>
-    )),
-    h2: React.forwardRef(({ children, ...props }: any, ref) => (
-      <h2 {...props} ref={ref}>{children}</h2>
-    )),
-    h3: React.forwardRef(({ children, ...props }: any, ref) => (
-      <h3 {...props} ref={ref}>{children}</h3>
-    )),
-    p: React.forwardRef(({ children, ...props }: any, ref) => (
-      <p {...props} ref={ref}>{children}</p>
-    )),
-    span: React.forwardRef(({ children, ...props }: any, ref) => (
-      <span {...props} ref={ref}>{children}</span>
-    )),
-    button: React.forwardRef(({ children, ...props }: any, ref) => (
-      <button {...props} ref={ref}>{children}</button>
-    )),
-    section: React.forwardRef(({ children, ...props }: any, ref) => (
-      <section {...props} ref={ref}>{children}</section>
-    )),
-    article: React.forwardRef(({ children, ...props }: any, ref) => (
-      <article {...props} ref={ref}>{children}</article>
-    )),
-    nav: React.forwardRef(({ children, ...props }: any, ref) => (
-      <nav {...props} ref={ref}>{children}</nav>
-    )),
-    li: React.forwardRef(({ children, ...props }: any, ref) => (
-      <li {...props} ref={ref}>{children}</li>
-    )),
-    ul: React.forwardRef(({ children, ...props }: any, ref) => (
-      <ul {...props} ref={ref}>{children}</ul>
-    )),
-  },
-  AnimatePresence: ({ children }: any) => <>{children}</>,
-  useScroll: () => ({ scrollYProgress: { get: () => 0 } }),
-  useTransform: () => 0,
-  useSpring: () => 0,
-}));
+vi.mock('framer-motion', () => {
+  const createMockComponent = (Tag: string) => {
+    const Component = React.forwardRef<HTMLElement, React.HTMLAttributes<HTMLElement>>(({ children, ...props }, ref) => {
+      return React.createElement(Tag, { ...props, ref }, children);
+    });
+    Component.displayName = `Motion${Tag}`;
+    return Component;
+  };
+
+  return {
+    motion: {
+      div: createMockComponent('div'),
+      h1: createMockComponent('h1'),
+      h2: createMockComponent('h2'),
+      h3: createMockComponent('h3'),
+      p: createMockComponent('p'),
+      span: createMockComponent('span'),
+      button: createMockComponent('button'),
+      section: createMockComponent('section'),
+      article: createMockComponent('article'),
+      nav: createMockComponent('nav'),
+      li: createMockComponent('li'),
+      ul: createMockComponent('ul'),
+    },
+    AnimatePresence: function AnimatePresence({ children }: { children: React.ReactNode }) { return <>{children}</>; },
+    useScroll: () => ({ scrollYProgress: { get: () => 0 } }),
+    useTransform: () => 0,
+    useSpring: () => 0,
+  };
+});
